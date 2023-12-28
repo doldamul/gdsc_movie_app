496e17fb0d20d5f11dc3f5cc329f976c76e9dc33
doldamul <junha0504@gmail.com>
2023-12-29 04:30:08 +0900

    3주차 클론 코딩.

---
+++ lib/constants/api_endpoints.dart
@@ -0,0 +1,6 @@
+class ApiEndPoints {
+  static const String tmdbImage500 = 'https://image.tmdb.org/t/p/w500';
+  static const String tmdbMovieList = 'https://api.themoviedb.org/3/movie';
+  static const String tmdbMovieTrendList = 'https://api.themoviedb.org/3/trending/movie';
+  static const String tmdbSearchMovie = 'https://api.themoviedb.org/3/search/movie';
+}
