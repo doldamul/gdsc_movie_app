f1f147c8dda5912ab3b5cb0ead8513b89ce31b11
doldamul <junha0504@gmail.com>
2023-12-29 19:59:11 +0900

    영화 검색 화면 썸네일 추가

--- lib/screens/search/widgets/search_listitem_widget.dart
+++ lib/screens/search/widgets/search_listitem_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gdsc_movie_app/constants/sizes.dart';
+import 'package:gdsc_movie_app/constants/gaps.dart';
 import 'package:gdsc_movie_app/models/tmdb/tmdb_movie_listitem_model.dart';
+import 'package:gdsc_movie_app/constants/api_endpoints.dart';
 
 class SearchListItemWidget extends StatelessWidget {
   final TMDBMovieListItemModel? movie;
@@ -14,12 +16,41 @@ class SearchListItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: Sizes.size72,
-      child: Center(
-        child: Text(
-          movie?.originalTitle ?? 'N/A',
-          style: const TextStyle(
-            color: Colors.white,
-          ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: Sizes.size3),
+        child: Row(
+          children: [
+            Gaps.h8,
+            if (movie != null && movie?.posterPath != 'null' && movie?.posterPath != '')
+              Row(
+                children: [
+                  Gaps.h8,
+                  Container(
+                      width: 50,
+                      padding: const EdgeInsets.symmetric(horizontal: Sizes.size3),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Sizes.size3),
+                        color: Colors.blueGrey.shade300,
+                      ),
+                      clipBehavior: Clip.hardEdge,
+                      child: Image.network(
+                        '${ApiEndPoints.tmdbImage500}/${movie!.posterPath}',
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.fitWidth,
+                      )
+                  ),
+                ],
+              ),
+            Gaps.h8,
+            Text(
+              movie?.originalTitle ?? 'N/A',
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Gaps.h8,
+          ],
         ),
       ),
     );
